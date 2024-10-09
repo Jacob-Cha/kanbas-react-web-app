0691b477e7c249cc3b80f2a8e0b6fbfcd0b37e46
Jacob Chamoun <78745671+Jacob-Cha@users.noreply.github.com>
2024-10-09 02:51:40 -0400

    Update Editor.tsx

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button, Row, Col } from "react-bootstrap"; // Import Bootstrap components
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 export default function AssignmentEditor() {
   return (
